1ae655781869a4517cfd39da203951f126fec75b
mdodge2 <mdodge@hawaii.edu>
2023-11-01 13:22:26 -1000

    add file time annotation to csv writer

--- write_run_to_csv.py
+++ write_run_to_csv.py
@@ -8,6 +8,8 @@ from run_types import VerboseRunData, TerseRunData
 from typing import Union
 
 def write_run_to_csv(mode: str, data: Union[list[VerboseRunData], list[TerseRunData]]) -> str:
-    """"""
+    """Writes an experiment run to a CSV on the local filesystem.\n
+    It names the file using ISO 8601 conventions\n
+    Example: viscometer_2023-11-01T13:19:02-10:00.csv"""
     
     os.path.abspath()
