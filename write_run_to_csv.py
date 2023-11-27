b0ea84f9d56348935acc709a9fb724395959ef67
mdodge2 <mdodge@hawaii.edu>
2023-11-27 10:45:33 -1000

    remove unnecessary imports

--- write_run_to_csv.py
+++ write_run_to_csv.py
@@ -1,16 +1,14 @@
 # Michael Dodge II
 # ME 482: Senior Design Project (Fall 2023)
 # Team Lava
-import io
 import csv
-import os
 from datetime import datetime
 from run_types import VerboseRunData, TerseRunData
 from typing import Union
 
 def write_run_to_csv(mode: str, data: Union[list[VerboseRunData], list[TerseRunData]], base_dir: str) -> str:
     """Writes an experiment run to a CSV on the local filesystem.\n
-    It names the file using ISO 8601 conventions\n
+    It names the file using ISO 8601 conventions.\n
     Example: viscometer_2023-11-01T13:19:02.csv"""
 
     # Get timestamp
