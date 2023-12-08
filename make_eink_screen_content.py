3e8407602ea4e9b0e7ef15a664e90117de4663f1
mdodge2 <mdodge@hawaii.edu>
2023-12-07 16:21:49 -1000

    make text clearer

--- make_eink_screen_content.py
+++ make_eink_screen_content.py
@@ -12,8 +12,8 @@ def make_content(run_data: dict) -> PapirusComposite:
     * GPS Coordinates [xx.xxxx, yy.yyyy]"""
     try: 
         screen_content = PapirusComposite(False, rotation = 0)
-        screen_content.AddText(f"Time (s): {run_data['time_since_power_on__ms']/1000.0}", 5, 5)
-        screen_content.AddText(f"Torque (raw): {run_data['torque_raw_10bit']}", 5, 30)
+        screen_content.AddText(f"Time (s): {run_data['time_since_power_on__ms']/1000.0}", x=5, y=5, size=24)
+        screen_content.AddText(f"Torque (raw): {run_data['torque_raw_10bit']}", x=5, y=34, size=24)
         return screen_content
     except Exception as e:
         screen_content = PapirusComposite(False, rotation = 0)
