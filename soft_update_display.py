9b89689449f505007b712e7efce82db333a0ccf4
mdodge2 <mdodge@hawaii.edu>
2023-12-07 15:29:24 -1000

    remove async attempt

--- soft_update_display.py
+++ soft_update_display.py
@@ -3,9 +3,8 @@
 # Team Lava
 from datetime import datetime, timedelta
 from papirus import PapirusComposite
-import asyncio
 
-async def soft_update_display(time_since_last_update: datetime, content: PapirusComposite) -> datetime:
+def soft_update_display(time_since_last_update: datetime, content: PapirusComposite) -> datetime:
     """Write a composite image to the display if it has been at least 0.5 seconds since time_since_last_update."""
 
     # if the time between time_since_last_update and now is less than 0.5 seconds, just exit the function
