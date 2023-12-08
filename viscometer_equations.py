b42dce9cc15f5ecac35ad4e002a8391d540ce829
mdodge2 <mdodge@hawaii.edu>
2023-12-07 15:11:09 -1000

    remove numpy

--- viscometer_equations.py
+++ viscometer_equations.py
@@ -1,7 +1,6 @@
 # Michael Dodge II
 # ME 482: Senior Design Project (Fall 2023)
 # Team Lava
-import numpy as np
 from typing import Literal
 
 def computeTorqueFrom10Bit(torque_10bit: int) -> float:
