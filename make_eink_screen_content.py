f64263634084d96929b477bd0e76b4ee1c7a3756
mdodge2 <mdodge@hawaii.edu>
2023-12-07 15:15:50 -1000

    fix missing return statement

--- make_eink_screen_content.py
+++ make_eink_screen_content.py
@@ -14,6 +14,7 @@ def make_content(run_data: dict) -> PapirusComposite:
         screen_content = PapirusComposite(False, rotation = 270)
         screen_content.AddText(f"Time: {run_data['time_since_power_on__ms']}", 5, 5)
         screen_content.AddText(f"Torque (10 bit): {run_data['torque_raw_10bit']}", 5, 30)
+        return screen_content
     except Exception as e:
         screen_content = PapirusComposite(False, rotation = 270)
         screen_content.AddText("Something went wrong.", 5, 5)
